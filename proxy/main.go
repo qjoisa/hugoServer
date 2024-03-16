40193208d2e4cacd12d118a3889c8afcc6cbbacc
qjoisa <ujmin04@gmail.com>
2024-03-16 16:04:41 +0500

    proxy main

--- proxy/main.go
+++ proxy/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chi/chi"
-	"log"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
-	"os"
-	"time"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 func main() {
 	r := chi.NewRouter()
+	proxy := NewReverseProxy("hugo", "1313")
 
-	// ...
+	r.Use(proxy.ReverseProxy)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/api/", APIHandler)
 
 	http.ListenAndServe(":8080", r)
 }
@@ -31,23 +35,38 @@ func NewReverseProxy(host, port string) *ReverseProxy {
 
 func (rp *ReverseProxy) ReverseProxy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
+		if strings.HasPrefix(r.URL.String(), "/api/") {
+			next.ServeHTTP(w, r)
+			return
+		} else {
+			rev := httputil.ReverseProxy{Rewrite: func(r *httputil.ProxyRequest) {
+				r.SetURL(&url.URL{Scheme: "http", Host: rp.host + ":" + rp.port})
+				r.SetXForwarded()
+			}}
+			rev.ServeHTTP(w, r)
+		}
 	})
 }
 
-const content = ``
-
-func WorkerTest() {
-	t := time.NewTicker(1 * time.Second)
-	var b byte = 0
-	for {
-		select {
-		case <-t.C:
-			err := os.WriteFile("/app/static/_index.md", []byte(fmt.Sprintf(content, b)), 0644)
-			if err != nil {
-				log.Println(err)
-			}
-			b++
-		}
-	}
+func APIHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello from API"))
 }
+
+//
+//const content = ``
+//
+//func WorkerTest() {
+//	t := time.NewTicker(1 * time.Second)
+//	var b byte = 0
+//	for {
+//		select {
+//		case <-t.C:
+//			err := os.WriteFile("/app/static/_index.md", []byte(fmt.Sprintf(content, b)), 0644)
+//			if err != nil {
+//				log.Println(err)
+//			}
+//			b++
+//		}
+//	}
+//}
